45eb1356fbe0b1dcf230b433f7388657e71f5ee1
UMCA\fsalazarmo <fsalazarmo@castrocarazo.ac.cr>
2026-07-16 10:46:04 -0600

    no message

--- PermisosPuestosApi/Controllers/PuestosController.cs
+++ PermisosPuestosApi/Controllers/PuestosController.cs
@@ -18,17 +18,25 @@ namespace PermisosPuestosApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetPuestos()
         {
-            var data = await _context.Puestos.FromSqlRaw("EXEC sp_GetPuestos").ToListAsync();
+            // Usamos el parámetro @Accion='SELECT'
+            var data = await _context.Puestos
+                .FromSqlRaw("EXEC sp_GestionarPuestos @Accion='SELECT'")
+                .ToListAsync();
             return Ok(data);
         }
 
         [HttpPost]
         public async Task<IActionResult> CreatePuesto([FromBody] Puesto p)
         {
-            var p1 = new SqlParameter("@CodigoPuesto", p.CodigoPuesto);
-            var p2 = new SqlParameter("@NombrePuesto", p.NombrePuesto);
-            var p3 = new SqlParameter("@Descripcion", p.Descripcion ?? (object)DBNull.Value);
-            await _context.Database.ExecuteSqlRawAsync("EXEC sp_CreatePuesto @CodigoPuesto, @NombrePuesto, @Descripcion", p1, p2, p3);
+            var accion = new SqlParameter("@Accion", "INSERT");
+            var cod = new SqlParameter("@CodigoPuesto", p.CodigoPuesto);
+            var nom = new SqlParameter("@NombrePuesto", p.NombrePuesto);
+            var desc = new SqlParameter("@Descripcion", (object)p.Descripcion ?? DBNull.Value);
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC sp_GestionarPuestos @Accion, @CodigoPuesto=@CodigoPuesto, @NombrePuesto=@NombrePuesto, @Descripcion=@Descripcion", 
+                accion, cod, nom, desc);
+            
             return Ok();
         }
 
@@ -36,19 +44,29 @@ namespace PermisosPuestosApi.Controllers
         public async Task<IActionResult> UpdatePuesto(int id, [FromBody] Puesto p)
         {
             if (id != p.Id) return BadRequest();
-            var pId = new SqlParameter("@Id", id);
-            var p1 = new SqlParameter("@CodigoPuesto", p.CodigoPuesto);
-            var p2 = new SqlParameter("@NombrePuesto", p.NombrePuesto);
-            var p3 = new SqlParameter("@Descripcion", p.Descripcion ?? (object)DBNull.Value);
-            await _context.Database.ExecuteSqlRawAsync("EXEC sp_UpdatePuesto @Id, @CodigoPuesto, @NombrePuesto, @Descripcion", pId, p1, p2, p3);
+
+            var accion = new SqlParameter("@Accion", "UPDATE");
+            var pid = new SqlParameter("@Id", id);
+            var cod = new SqlParameter("@CodigoPuesto", p.CodigoPuesto);
+            var nom = new SqlParameter("@NombrePuesto", p.NombrePuesto);
+            var desc = new SqlParameter("@Descripcion", (object)p.Descripcion ?? DBNull.Value);
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC sp_GestionarPuestos @Accion, @Id=@Id, @CodigoPuesto=@CodigoPuesto, @NombrePuesto=@NombrePuesto, @Descripcion=@Descripcion", 
+                accion, pid, cod, nom, desc);
+
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePuesto(int id)
         {
-            var pId = new SqlParameter("@Id", id);
-            await _context.Database.ExecuteSqlRawAsync("EXEC sp_DeletePuesto @Id", pId);
+            var accion = new SqlParameter("@Accion", "DELETE");
+            var pid = new SqlParameter("@Id", id);
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC sp_GestionarPuestos @Accion, @Id=@Id", accion, pid);
+            
             return NoContent();
         }
     }
